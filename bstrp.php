a441ba6891b9d4f6d1e347958797038ed9bcac81
areen <areen_darawsheh@hotmail.com>
2014-01-29 02:31:41 +0300

    yyy

--- bstrp.php
+++ bstrp.php
@@ -32,7 +32,39 @@
 
 
 <?php
-	echo'Hello World ... ';
+  include_once('http://areen.dc7.us/confg.php') ;
+          $sql_filter_01= mysql_real_escape_string($_POST["hihi"]);
+         
+          $sql_filter_02 = mysql_real_escape_string($_POST["my_name"]);
+          
+          
+        $sql_txt_01  = "SELECT course_name, mark_first, mark_second,mark_other,mark_final,(mark_first+mark_second+mark_other) mark FROM stdinfo
+                        WHERE course_semester = '" . $sql_filter_01 . "' AND student_id = '" . $sql_filter_02 . "'";
+        
+        $sql_result_01 =  mysql_query($sql_txt_01);
+
+                if($sql_result_01 === FALSE)
+                    {
+                       die(mysql_error()); // TODO: better error handling
+                   }
+                   
+                   	while (   $row2 = mysql_fetch_array($sql_result_01))	
+					{
+						echo"<div class='panel-group' id='accordion'>
+							<div class='panel panel-default'>
+								<div class='panel-heading'>
+									<h4 class='panel-title'>
+						                <a class='accordion-toggle' data-toggle='collapse' data-parent='#accordion' href='#panel1'>". $row2["course_name"]."</a>
+									</h4>
+								</div>
+								<div id='panel1' class='panel-collapse collapse'>
+									<div class='panel-body'>
+						             First ". $row2["mark_first"]."
+									</div>
+								</div>
+						    </div>
+						";
+					}
 
 	echo"	<div class='panel-group' id='accordion'>
 							<div class='panel panel-default'>
